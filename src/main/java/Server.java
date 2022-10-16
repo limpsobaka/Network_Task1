6b88cea81b5cf6d23a95ab9781227b2dfc667081
Limpsobaka <limpsobaka@yandex.ru>
2022-10-16 16:28:55 +0300

    Change Server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -8,14 +8,16 @@ import java.net.Socket;
 public class Server {
   private static final int PORT = 8080;
 
-  public static void main(String[] args) throws IOException {
-    ServerSocket serverSocket = new ServerSocket(PORT);
+  public static void main(String[] args) {
+    try (ServerSocket serverSocket = new ServerSocket(PORT);
     Socket clientSocket = serverSocket.accept();
     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-    System.out.println("New connection accepted");
-    final String name = in.readLine();
-    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+      System.out.println("New connection accepted");
+      final String name = in.readLine();
+      out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+    } catch (IOException e) {
+      System.out.println("Произошла ошибка: " + e.getMessage());
+    }
   }
 }
